61aa8e069319190c6f39bc17dfc8a60b06bd9c77
PriyankaReddy <priyanka.anf227@gmail.com>
2022-10-26 20:51:44 -0500

    updated step 5

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var times=0;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next)
+ {
+  times++;
+  res.send('User accesses are:'+times);
 });
 
 module.exports = router;
