fbe84cf1117fb7b91f4193bf2b7c53f6daa01cac
PriyankaReddy <priyanka.anf227@gmail.com>
2022-10-26 21:05:10 -0500

    updated step 6

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+          // Build a fake url so we can get the search parameters using a URL object
+          fake_url = "https://fake.com/path" + req.url
+          const url = new URL(fake_url)
+          const search_params = url.searchParams
+    
+          var x = Math.random();
+    
+    
+          if (search_params.get("x")) {
+            x= search_params.get("x");}
+   
+    var d = Math.fround(x);
+    var e = Math.random(x);
+    var f = Math.round(x);
+  res.render('computation', { title: 'Express',x:x,d:d,e:e,f:f});
+});
+
+module.experts.router;
